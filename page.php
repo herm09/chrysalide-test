f9cb9293dec11f0dc482725ed4d3598da0f5502e
Hermione Tetard <herm.tetard@gmail.com>
2023-11-27 16:56:29 +0100

    modif page.php

--- page.php
+++ page.php
@@ -32,27 +32,18 @@ get_header();
 		endwhile; // End of the loop.
 		?>
 		
-		<div class="text">
-				<p>
-				Bienvenue sur Ping Passion, votre destination en ligne pour tout ce qui concerne le tennis de table ! Que vous 
-				soyez un joueur débutant, un amateur passionné ou un professionnel chevronné, notre site est conçu pour vous offrir une 
-				expérience riche en informations, ressources et inspiration pour améliorer vos compétences et votre passion pour ce 
-				sport dynamique.
-				<br>
-				<h3>À propos de nous :</h3>
-				Ping Passion a été créé par une équipe de passionnés du tennis de table déterminés à promouvoir et à développer ce 
-				sport fascinant. Notre objectif est de fournir des ressources de qualité, de stimuler la croissance de la communauté de 
-				joueurs et d'inspirer une nouvelle génération de pongistes.
-				<br>
-				Rejoignez-nous sur Ping Passion et faites partie de cette aventure passionnante du tennis de table. Que vous soyez 
-				un joueur en herbe ou un vétéran de la table, nous sommes là pour nourrir votre passion et améliorer vos 
-				compétences. Vivez, respirez et jouez au ping-pong avec Ping Passion !
-				<br><br>
-				Visitez notre site dès maintenant : www.pingpassion.com
-				<br><br>
-				Suivez-nous sur les réseaux sociaux : Facebook, Twitter, Instagram.
-				</p>
-			</div>
+
+		<h1>Nos actualités</h1>
+
+		<div class="actus">
+
+			<img src="" alt="">
+
+			<img src="" alt="">
+
+			<img src="" alt="">
+
+		</div>
 
 	</main><!-- #main -->
 
