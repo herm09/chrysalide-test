29a63a863640bdc38d4d5bbc594b852a0197f45a
Hermione Tetard <herm.tetard@gmail.com>
2023-11-27 17:45:36 +0100

    modif images page.php

--- page.php
+++ page.php
@@ -41,7 +41,7 @@ get_header();
 
 			<img src="<?php echo get_template_directory_uri(); ?>/img/humains_heureux.png" alt="Hommes heureux" class="hommesHeureux">
 
-			<img src="<?php echo get_template_directory_uri(); ?>/img/autistes.youpi.png" alt="Happy" class="Happy">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/autistes_youpi.png" alt="Happy" class="Happy">
 
 		</div>
 
