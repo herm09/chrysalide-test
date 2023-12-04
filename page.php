34b75b9a13aa18bca536473faf62b8e07de2f0e1
Timothée de Vasselot <timothee.de_vasselot@edu.devinci.fr>
2023-12-04 17:11:07 +0100

    page.php

--- page.php
+++ page.php
@@ -31,6 +31,68 @@ get_header();
 		endwhile; // End of the loop.
 		?>
 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon Site</title>
+    <style>
+        /* Ajoutez ici votre style CSS si nécessaire */
+    </style>
+</head>
+<body>
+
+    <!-- Section Carrousel -->
+    <section id="carrousel">
+        <!-- Contenu du carrousel va ici -->
+    </section>
+
+    <!-- Section Concept -->
+    <section id="concept">
+        <h2>Notre concept</h2>
+        <div class="div-concept">
+            <div class="div-inclu">
+                <img src="inclu.jpg" alt="Inclusivité" class="img-inclu">
+                <h3>Inclusivité</h3>
+                <p>Description de l'inclusivité...</p>
+            </div>
+            <div class="div-eco">
+                <img src="eco.jpg" alt="Ecologie" class="img-eco">
+                <h3>Ecologie</h3>
+                <p>Description de l'écologie...</p>
+            </div>
+            <div class="div-techno">
+                <img src="techno.jpg" alt="Technologie" class="img-techno">
+                <h3>Technologie</h3>
+                <p>Description de la technologie...</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Ajoutez ici d'autres sections si nécessaire -->
+
+	<section id="adherer">
+        <div class="div-adherer">
+            <div class="div-besoin">
+                <p>NOUS AVONS BESOIN DE VOUS</p>
+            </div>
+            <div class="div-rejoindre">
+                <p>Rejoignez-nous en devenant adhérent</p>
+            </div>
+            <div class="div-bouton">
+                <button>Voir plus</button>
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
+
+
+
+
+
 	</main><!-- #main -->
 
 <?php
