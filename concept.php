2ba1c165d86dd6bf4f9c00b73a64d67125332c8b
Timothée de Vasselot <timothee.de_vasselot@edu.devinci.fr>
2023-12-07 18:17:36 +0100

    page concept/valeurs

---
+++ concept.php
@@ -0,0 +1,113 @@
+<?php
+// get_header();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600;1000&family=Poppins:wght@500;600&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <main>
+
+        <section class="concept">
+
+
+        </section>
+
+        <section class="inclusivité">
+            <div class="div-inclu1">
+
+                <div class="valeurs-inclu">
+                    <div class="valeurs-inclu1">
+                        <h1>Nos valeurs</h1> <br>
+                        <h3>Inclusivité</h3>
+                    </div>
+                    <div class="valeurs-inclu2">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+
+                </div>
+
+                <div class="valeurs-img">
+                    <img src="images/Rectangle-119.png" alt="">
+                </div>
+            </div>
+
+            <div class="div-inclu2">
+                <div class="schema-inclu">
+                    <img src="images/maison-1.png" alt="">
+                </div>
+            </div>
+
+        </section>
+
+        <section class="écologie">
+
+            <div class="div-eco">
+                <div class="valeurs-img-eco">
+                    <img src="images/Rectangle-119.png" alt="">
+                </div>
+                <div class="valeurs-eco">
+                    <div class="valeurs-eco1">
+                        <h1>Nos valeurs</h1> <br>
+                        <h3>Ecologie</h3>
+
+                    </div>
+                    <div class="valeurs-eco2">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic odio repellat eum earum corrupti
+                            harum pariatur voluptatum voluptatem magni! Repellendus, earum atque voluptate culpa eius
+                            blanditiis. Esse officia earum sequi debitis aspernatur explicabo quam consectetur dolorem,
+                            quae, maiores assumenda ab!</p>
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+        <section class="technologie">
+
+            <div class="div-techno">
+                
+                <div class="valeurs-techno">
+                    <div class="valeurs-techno1">
+                        <h1>Nos valeurs</h1> <br>
+                        <h3>Technologies</h3>
+
+                    </div>
+                    <div class="valeurs-techno2">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic odio repellat eum earum corrupti
+                            harum pariatur voluptatum voluptatem magni! Repellendus, earum atque voluptate culpa eius
+                            blanditiis. Esse officia earum sequi debitis aspernatur explicabo quam consectetur dolorem,
+                            quae, maiores assumenda ab!</p>
+                    </div>
+
+                </div>
+
+                <div class="valeurs-img">
+                    <img src="images/Rectangle-119.png" alt="">
+                </div>
+            </div>
+
+        </section>
+
+    </main>
+
+</body>
+
+<?php
+// get_footer();
+?>
+
+</html>
