80647ef9ec31b52b733a325cc1e1fa2ee44c7f7c
Timothée de Vasselot <timothee.de_vasselot@edu.devinci.fr>
2023-12-13 15:22:12 +0100

    commit actualite.php/.css and commit concept.php/.css

---
+++ actualite.php
@@ -0,0 +1,169 @@
+<?php
+
+// get_header();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Paytone+One&display=swap" rel="stylesheet">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <main>
+        <div class="overlay"></div>
+
+        <section class="nos-actualites">
+            <div class="bloc-pres">
+                <div class="pres-h1">
+                    Nos Actualités
+                </div>
+                <div class="pres-p">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, ratione provident dicta beatae saepe
+                    minus ad ducimus numquam ipsam quaerat, earum vitae quia quod suscipit. Placeat atque modi at, amet
+                    eveniet eaque consectetur quisquam natus alias fuga perspiciatis rerum in pariatur nisi, recusandae
+                    incidunt impedit facere! Quidem consequuntur non reiciendis!
+                </div>
+                <div class="pres-btn">
+                    <button>VOIR PLUS</button>
+                </div>
+            </div>
+
+
+            <div class="vague-blanche">
+
+            </div>
+
+        </section>
+
+        <section class="actualités-1">
+            <div class="actu-1">
+
+                <div class="img-actu1">
+                    <img src="images/carre-actu.png" alt="">
+                </div>
+                <div class="actu1-h1">
+                    <h1>Actualité 1.</h1>
+                </div>
+                <div class="actu1-p">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt assumenda non rerum eveniet
+                        distinctio dolores reprehenderit similique optio sint, quisquam architecto reiciendis ducimus,
+                        cupiditate hic suscipit sequi recusandae ex amet?</p>
+                </div>
+
+            </div>
+            <div class="actu-2">
+
+                <div class="img-actu2">
+                    <img src="images/carre-actu2.png" alt="">
+                </div>
+                <div class="actu2-h1">
+                    <h1>Actualité 2.</h1>
+                </div>
+
+            </div>
+
+            <div class="actu-3">
+
+                <div class="img-actu3">
+                    <img src="images/carre-actu2.png" alt="">
+                </div>
+                <div class="actu3-h1">
+                    <h1>Actualité 3.</h1>
+                </div>
+
+            </div>
+
+        </section>
+
+        <section class="actualités-2">
+            <div class="actus-6à9">
+                <div class="actu-6">
+                    <h2>Actualité 6.</h2>
+                </div>
+                <div class="actu-7">
+                    <h2>Actualité 7.</h2>
+                </div>
+                <div class="actu-8">
+                    <h2>Actualité 8.</h2>
+                </div>
+                <div class="actu-9">
+                    <h2>Actualité 9.</h2>
+                </div>
+
+            </div>
+            <div class="actu-4">
+
+                <div class="img-actu4">
+                    <img src="images/carre-actu3.png" alt="">
+                </div>
+                <div class="actu4-h1">
+                    <h1>Actualité 4.</h1>
+                </div>
+
+            </div>
+            <div class="actu-5">
+
+                <div class="img-actu5">
+                    <img src="images/carre-actu3.png" alt="">
+                </div>
+                <div class="actu5-h1">
+                    <h1>Actualité 5.</h1>
+                </div>
+
+            </div>
+
+        </section>
+
+
+        <section class="actualités-3">
+            <div class="actu-10">
+                <div class="img-actu10">
+                    <img src="images/carre-actu4.png" alt="">
+                    <h1>Actualité 10.</h1>
+                </div>
+            </div>
+
+            <div class="actu-11">
+                <div class="img-actu11">
+                    <img src="images/carre-actu4.png" alt="">
+                    <h1>Actualité 11.</h1>
+                </div>
+            </div>
+
+            <div class="actu-12">
+                <div class="img-actu12">
+                    <img src="images/carre-actu4.png" alt="">
+                    <h1>Actualité 12.</h1>
+                </div>
+            </div>
+
+            
+            <div class="actu-13">
+                <div class="img-actu13">
+                    <img src="images/carre-actu4.png" alt="">
+                    <h1>Actualité 13.</h1>
+                </div>
+            </div>
+
+
+
+
+        </section>
+
+    </main>
+</body>
+
+<?php
+// get_footer();
+?>
+
+</html>
--- concept.php
+++ concept.php
@@ -1,5 +1,5 @@
 <?php
- get_header();
+//  get_header();
 
 ?>
 
@@ -13,7 +13,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Paytone+One&display=swap" rel="stylesheet">
-   
+
 </head>
 
 <body>
@@ -29,6 +29,9 @@
                         blanditiis. Esse officia earum sequi debitis aspernatur explicabo quam consectetur dolorem,
                         quae, maiores assumenda ab!</p>
                 </div>
+                <div class="pres-btn">
+                <button>VOIR PLUS</button>
+            </div>
             </div>
 
 
@@ -39,10 +42,16 @@
             <div class="div-inclu1">
 
                 <div class="valeurs-inclu">
-                    <div class="valeurs-inclu1">
-                        <h1>Nos valeurs.</h1> 
-                        <h3>Inclusivité</h3>
+                    <div class="inclu-flex">
+                        <div class="valeurs-inclu1">
+                            <h1>Nos valeurs.</h1>
+                            <h3>Inclusivité</h3>
+                        </div>
+                        <div class="inclu-papillon">
+                            <img src="images/papillon2jaune.png" class="inclu-papillon-img" alt="">
+                        </div>
                     </div>
+
                     <div class="valeurs-inclu2">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
                             labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
@@ -51,12 +60,15 @@
 
                 </div>
 
-                <div class="valeurs-img">
-                    <img src="images/inclu-img.png" alt="">
+                <div class="inclu-img">
+                    <div class="inclu-overlay"></div>
+                    <img src="images/inclu-img.png" alt="" class="inclu-img-1">
                 </div>
             </div>
 
+
             <div class="div-inclu2">
+
                 <div class="schema-inclu">
                     <img src="images/maison-1.png" alt="">
                 </div>
@@ -67,13 +79,20 @@
         <section class="écologie">
 
             <div class="div-eco">
-                <div class="valeurs-img-eco">
-                    <img src="images/eco-img.png" alt="">
+                <div class="img-eco">
+                    <img src="images/eco-img.png" alt="" class="img-eco-1">
                 </div>
                 <div class="valeurs-eco">
-                    <div class="valeurs-eco1">
-                        <h1>Nos valeurs.</h1> 
-                        <h3>Ecologie</h3>
+
+                    <div class="eco-flex">
+                        <div class="eco-papillon">
+                            <img src="images/papillon2vert.png" class="eco-img-1" alt="">
+                        </div>
+
+                        <div class="valeurs-eco1">
+                            <h1>Nos valeurs.</h1>
+                            <h3>Ecologie</h3>
+                        </div>
 
                     </div>
                     <div class="valeurs-eco2">
@@ -92,44 +111,55 @@
             <div class="div-techno">
 
                 <div class="valeurs-techno">
-                    <div class="valeurs-techno1">
-                        <h1>Nos valeurs.</h1> 
-                        <h3>Technologies</h3>
-
+                    <div class="techno-flex1">
+                        <div class="valeurs-techno1">
+                            <h1>Nos valeurs.</h1>
+                            <h3>Technologies</h3>
+                        </div>
+                        <div class="techno-papillon">
+                            <img src="images/papillon2rose.png" alt="">
+                        </div>
                     </div>
+
+
                     <div class="valeurs-techno2">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem labore harum fugit consequatur possimus distinctio ut excepturi eius delectus, esse reprehenderit blanditiis quidem natus alias quisquam expedita reiciendis quod laboriosam laborum! Neque, non ipsam. Praesentium laboriosam eum eligendi corrupti ad velit numquam qui ut vero aliquid debitis, dolores dolorum iure aut molestias nihil tempore et unde reiciendis ipsum excepturi, omnis nisi? Molestias mollitia corporis ad libero illo tempora distinctio aspernatur.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem labore harum fugit
+                            consequatur possimus distinctio ut excepturi eius delectus, esse reprehenderit blanditiis
+                            quidem natus alias quisquam expedita reiciendis quod laboriosam laborum! Neque, non ipsam.
+                            Praesentium laboriosam eum eligendi corrupti ad velit numquam qui ut vero aliquid debitis,
+                            dolores dolorum iure aut molestias nihil tempore et unde reiciendis ipsum excepturi, omnis
+                            nisi? Molestias mollitia corporis ad libero illo tempora distinctio aspernatur.</p>
                     </div>
 
                 </div>
 
-                <div class="valeurs-img">
-                    <img src="images/techno-img.png" alt="">
+                <div class="techno-img">
+                    <img src="images/techno-img.png" alt="" class="techno-img-1">
                 </div>
             </div>
 
         </section>
 
         <section id="adherer">
-        <div class="div-adherer">
-            <div class="div-besoin">
-                <p>NOUS AVONS BESOIN DE VOUS</p>
+            <div class="div-adherer">
+                <div class="div-besoin">
+                    <p>NOUS AVONS BESOIN DE VOUS</p>
+                </div>
+                <div class="div-rejoindre">
+                    <p>REJOIGNEZ-NOUS EN DEVENANT ADHERENT.</p>
+                </div>
+                <div class="div-bouton">
+                    <button>Voir plus</button>
+                </div>
             </div>
-            <div class="div-rejoindre">
-                <p>REJOIGNEZ-NOUS EN DEVENANT ADHERENT.</p>
-            </div>
-            <div class="div-bouton">
-                <button>Voir plus</button>
-            </div>
-        </div>
-    </section>
+        </section>
 
     </main>
 
 </body>
 
 <?php
-get_footer();
+// get_footer();
 ?>
 
 </html>
